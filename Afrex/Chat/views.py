e03a6ce6d0316814e04fa3b2b463ffa29a8a46f5
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-02 16:54:23 +0100

    I am currently working on the users registration and login form

--- Afrex/Chat/views.py
+++ Afrex/Chat/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from datetime import datetime
+from django.utils import timezone
+import random
+from django.db.models import Q
+
+# this modules below is for user authentication part and registration
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 
 # Create your views here.
