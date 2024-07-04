1123ddceccbc7d829070ba14e136964bae4f3006
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-04 19:51:29 +0100

    hotfix

--- Afrex/tailwind.config.js
+++ Afrex/tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
 	  /* this above is the path to the directory of my template(html) files*/
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '120': '40rem', //customizing my height value
+      },
+      width: {
+        '120': '40rem', //customized my width value
+      },
+      colors: {
+        'translucent-gray': 'rgba(110, 110, 110, 0.25)', //  i intend to use this for the login and signup page
+        'coral-pink': '#ff7474',
+      },
+      fontFamily: {
+        'magnolia': ['Magnolia Script', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
