229c5184b652e110103f791e1814b129b4d3e471
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-04 10:54:07 +0100

    I just finished installing and adding all necessary dependency that this project will be needing for the smooth running of tailwindcss

---
+++ Afrex/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	  './Chat/templates/pages/*.html',
+	  /* this above is the path to the directory of my template(html) files*/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
