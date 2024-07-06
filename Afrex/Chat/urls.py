9e850b9d72942bdacbf1ef745cc57f6aae466b14
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-06 22:32:19 +0100

    I worked on the first page of the password reset page, everything is now working fine, I also added the password_reset_done,html that is a page that render a confirmation message that a link to proceed the password reset have been sent to your email

--- Afrex/Chat/urls.py
+++ Afrex/Chat/urls.py
@@ -2,19 +2,15 @@ from django.urls import path, include
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
-from django.contrib.auth.views import (
-    PasswordResetView, 
-    PasswordResetDoneView, 
-    PasswordResetConfirmView,
-    PasswordResetCompleteView
-)
+from django.contrib.auth import views as auth
+
 
 urlpatterns = [
     path("signup/", views.sign_up, name="signup"),
     path("login/", views.sign_in, name="login"),
     path("logout/", views.sign_out, name="logout"),
-    path('password-reset/', PasswordResetView.as_view(template_name='pages/password_reset.html'), name='password-reset'),
-    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='pages/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='pages/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='pages/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset/', auth.PasswordResetView.as_view(template_name = 'pages/password_reset.html'), name = 'password_reset'),
+    path('password-reset/done/', auth.PasswordResetDoneView.as_view(template_name = 'pages/password_reset_done.html'), name = 'password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth.PasswordResetConfirmView.as_view(template_name='pages/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/', auth.PasswordResetCompleteView.as_view(template_name = 'pages/password_reset_complete.html'), name = 'password_reset_complete'),
 ]
