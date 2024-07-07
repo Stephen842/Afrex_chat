9c0c2e7f4060480fdfedc713e5a29427800cfb3c
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-07 14:21:53 +0100

    Hot fix

--- Afrex/Afrex/settings.py
+++ Afrex/Afrex/settings.py
@@ -134,7 +134,7 @@ STORAGES = {
     },
 }
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
