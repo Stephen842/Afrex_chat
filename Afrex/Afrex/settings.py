3351fd91b5445a7a0c341bb3ae4da395669654d2
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-09 17:22:43 +0100

    hot fix

--- Afrex/Afrex/settings.py
+++ Afrex/Afrex/settings.py
@@ -134,7 +134,12 @@ STORAGES = {
     },
 }
 
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = '/'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
