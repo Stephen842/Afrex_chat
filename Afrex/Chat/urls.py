6fd8665e2b899cad5e65f0a9b2ec69d71fc2e8eb
Stephen842 <ugotachisomstephen@gmail.com>
2024-09-24 15:41:23 +0100

    I have been working on my live server in ensuring that this project is also live while also I wlrk on the local copy, that is why it really must have been a long time since i last pushed, anyway alot have been done especially on my live server but the recent of them is the creation of a customize password reset email.

--- Afrex/Chat/urls.py
+++ Afrex/Chat/urls.py
@@ -4,13 +4,20 @@ from . import views
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth
 
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.sign_up, name='signup'),
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
-    path('password-reset/', auth.PasswordResetView.as_view(template_name = 'pages/password_reset.html'), name = 'password_reset'),
+
+    path('password-reset/',
+         auth.PasswordResetView.as_view(
+            template_name = 'pages/password_reset.html',
+            html_email_template_name='pages/password_reset_email.html'
+        ),
+        name = 'password_reset'
+    ),
+
     path('password-reset/done/', auth.PasswordResetDoneView.as_view(template_name = 'pages/password_reset_done.html'), name = 'password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth.PasswordResetConfirmView.as_view(template_name='pages/password_reset_confirm.html'),name='password_reset_confirm'),
     path('password-reset-complete/', auth.PasswordResetCompleteView.as_view(template_name = 'pages/password_reset_complete.html'), name = 'password_reset_complete'),
